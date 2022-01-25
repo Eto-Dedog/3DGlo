41f5dba48812a2f1fd6232f03cb19c53af147826
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-01-25 11:30:17 +0300

    Complet Lesson21
    Complet Lesson21

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import valid from './modules/valid'
+
 timer('31 january 2022')
 menu()
 modal()
+valid()
