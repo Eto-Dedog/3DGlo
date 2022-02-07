a1a2f32f9e4be49dc68f20b82d9e7424d66b845e
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-07 11:55:33 +0300

    Complet Lesson27--
    Complet Lesson27--

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import valid from './modules/valid'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
-timer('31 january 2022')
+timer('28  2022')
 menu()
 modal()
 valid()
 tabs()
 slider()
 calc()
+sendForm({
+    formId: 'form1',
+    someElem : [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
 
