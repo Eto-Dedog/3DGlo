984e43689c4784046405bfa5bdef842799ada3e2
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-07 12:14:41 +0300

    Complet Lesson27--
    Complet Lesson27--

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import slider from './modules/slider'
 import calc from './modules/calc'
 import sendForm from './modules/sendForm'
 
-timer('28  2022')
+timer('28 feb 2022')
 menu()
 modal()
 valid()
