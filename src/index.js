febf5bd6d3dee3fe7c52ae3cb56f73c4dc51a3bd
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-08 12:39:21 +0300

    Complet Lesson22
    Complet Lesson22

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import valid from './modules/valid'
 import tabs from './modules/tabs'
+import scroll from './modules/scroll'
 
 timer('28 feb 2022')
 menu()
 modal()
 valid()
 tabs()
+scroll()
