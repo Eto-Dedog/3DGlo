c00d8d937d452da7a0b6a75070d3cdc1cd039686
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-08 12:36:06 +0300

    Complet Lesson 19-20
    Complet Lesson 19-20

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import scroll from './modules/scroll'
 timer('28 feb 2022')
 menu()
 modal()
+scroll()
