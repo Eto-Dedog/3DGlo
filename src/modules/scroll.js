610b94567d69ad7353535cae8a893dc9c6472e64
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-09 10:26:35 +0300

    Complet Lesson27
    Complet Lesson27

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,5 @@
 const scroll = () => {
-    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    const smoothLinks = document.querySelectorAll('a[href^="#service-block"], a[href^="#portfolio"], a[href^="#calc"], a[href^="#command"], a[href^="#connect"]');
     for (let smoothLink of smoothLinks) {
         smoothLink.addEventListener('click', function (e) {
             e.preventDefault();
